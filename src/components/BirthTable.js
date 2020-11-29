e9d0bf41fa32a70fdec00ef3b6e63f057f056dcc
dariuszkorolczukcom <dkorolczuk86@gmail.com>
2020-11-29 14:46:24 +0000

    v0.1

--- src/components/BirthTable.js
+++ src/components/BirthTable.js
@@ -3,12 +3,16 @@ import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
+import SwipeableViews from 'react-swipeable-views';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import moment from 'moment';
 import { birthdayInfo } from '../util/info'
+
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
     table: {
@@ -18,13 +22,22 @@ const useStyles = makeStyles({
       width: "50%",
     }
   });
+
+function TabContainer({ children }) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {children}
+    </Typography>
+  );
+}
   
 export default function BirthTable(props) {
     const classes = useStyles();
-    const [birthdays, setDecimalBirthdays] = React.useState(null);
+    const [birthdays, setBirthdays] = React.useState(null);
+    const [value, setValue] = React.useState(0);
   
     React.useEffect(() => {
-      setDecimalBirthdays(props.birthdays)
+      setBirthdays(props.birthdays)
     },[props.birthdays])
   
     const parseTimeSinceBirthday = (value,tableValue) => {
@@ -35,31 +48,59 @@ export default function BirthTable(props) {
       var a = moment(value[1])
       return a.format('Do MMM YYYY')
     }
+
+
+    function handleChange(event, newValue) {
+      setValue(newValue);
+    }
+  
+    function handleChangeIndex(index) {
+      setValue(index);
+    }
   
     return (
-        <>
-          {birthdays != null &&
-            <TableContainer className={classes.container} component={Paper}>
-            {Object.entries(birthdays).map((tableValue, tableKey) => (
-              tableValue[0] === "birthdayDate" ? "" :
-              <Table key={tableKey} className={classes.table} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>{birthdayInfo[tableValue[0]].birthName}</TableCell>
-                    <TableCell align="right">{birthdayInfo[tableValue[0]].header}</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {Object.entries(tableValue[1]).map((value, key) => (
-                    <TableRow key={key}>
-                      <TableCell align="left">{parseTimeSinceBirthday(value,tableValue)}</TableCell>
-                      <TableCell align="right">{parseBirthdayDate(value)}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>))}
-            </TableContainer>
-          }
+        <>  
+        {birthdays != null && 
+          <>
+            <AppBar position="static" color="default">
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                variant={null}
+                >
+                {Object.entries(birthdays).map((tableValue, tableKey) => 
+                    <Tab label={birthdayInfo[tableValue[0]].birthName} />
+                )}
+              </Tabs>
+            </AppBar>
+            <SwipeableViews
+              axis={'x'}
+              index={value}
+              onChangeIndex={handleChangeIndex}
+              >
+              {Object.entries(birthdays).map((tableValue, tableKey) => 
+                  <TabContainer>
+                    <Table key={tableKey} className={classes.table} aria-label="simple table">
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>{birthdayInfo[tableValue[0]].header1}</TableCell>
+                          <TableCell align="right">{birthdayInfo[tableValue[0]].header2}</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {Object.entries(tableValue[1]).map((value, key) =>
+                            <TableRow key={key}>
+                              <TableCell align="left">{parseTimeSinceBirthday(value,tableValue)}</TableCell>
+                              <TableCell align="right">{parseBirthdayDate(value)}</TableCell>
+                            </TableRow>
+                        )}
+                      </TableBody>
+                  </Table>
+                </TabContainer>)}
+            </SwipeableViews>
+          </>}
         </>
     );
   }
--- src/util/info.js
+++ src/util/info.js
@@ -4,19 +4,22 @@ export const birthdayInfo = {
     },
     hoursDecimal: {
       birthName: "Birth Hours",
-      header: "Decimal Hours Birthday",
+      header1: "Hours since you were born",
+      header2: "BirthHoursDay",
       def: "your birthday once every 100 thousand hours of your life!",
       symbol: "hours",
     },
     minutesDecimal: {
       birthName: "Birth Minutes",
-      header: "Decimal Minutes Birthday",
+      header1: "Minutes since you were born",
+      header2: "BirthMinutesDay",
       def: "your birthday once every million hours of your life!",
       symbol: "minutes",
     },
     secondsDecimal: {
       birthName: "Birth Seconds",
-      header: "Decimal Seconds Birthday",
+      header1: "Seconds since you were born",
+      header2: "BirthSecondsDay",
       def: "your birthday once every 100 million hours of your life!",
       symbol: "seconds",
     }
