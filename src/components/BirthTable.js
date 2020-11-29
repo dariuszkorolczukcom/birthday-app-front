96b9f093be7c628c49920f5e29a1092fdaf07a07
dariuszkorolczukcom <dkorolczuk86@gmail.com>
2020-11-29 15:39:27 +0000

    v0.1: styles

--- src/components/BirthTable.js
+++ src/components/BirthTable.js
@@ -6,101 +6,104 @@ import TableCell from '@material-ui/core/TableCell';
 import SwipeableViews from 'react-swipeable-views';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import moment from 'moment';
-import { birthdayInfo } from '../util/info'
-
+import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import moment from 'moment';
+import { birthdayInfo } from '../util/info'
 
-const useStyles = makeStyles({
-    table: {
-      minWidth: 300,
-    },
-    container: {
-      width: "50%",
-    }
-  });
+const useStyles = makeStyles(() => ({
+  // root: {
+  //   backgroundColor: theme.palette.background.paper,
+  //   width: 500,
+  // },
+  table: {
+    minWidth: 300,
+  },
+  container: {
+    width: "50%",
+  },
+  bar: {
+    boxShadow: "0px 0px 1px 0px rgba(0,0,0,0.4)",
+  }
+}));
 
 function TabContainer({ children }) {
   return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
-      {children}
-    </Typography>
+    <Box p={1}>
+        {children}
+    </Box>
   );
 }
   
 export default function BirthTable(props) {
-    const classes = useStyles();
-    const [birthdays, setBirthdays] = React.useState(null);
-    const [value, setValue] = React.useState(0);
+  const classes = useStyles();
+  const [birthdays, setBirthdays] = React.useState(null);
+  const [value, setValue] = React.useState(0);
   
-    React.useEffect(() => {
-      setBirthdays(props.birthdays)
-    },[props.birthdays])
+  React.useEffect(() => {
+    setBirthdays(props.birthdays)
+  },[props.birthdays])
   
-    const parseTimeSinceBirthday = (value,tableValue) => {
-      return parseInt(value[0]).toLocaleString('en-US') + " " + birthdayInfo[tableValue[0]].symbol
-    }
-  
-    const parseBirthdayDate = (value) => {
-      var a = moment(value[1])
-      return a.format('Do MMM YYYY')
-    }
-
-
-    function handleChange(event, newValue) {
-      setValue(newValue);
-    }
-  
-    function handleChangeIndex(index) {
-      setValue(index);
-    }
-  
-    return (
-        <>  
-        {birthdays != null && 
-          <>
-            <AppBar position="static" color="default">
-              <Tabs
-                value={value}
-                onChange={handleChange}
-                indicatorColor="primary"
-                textColor="primary"
-                variant={null}
-                >
-                {Object.entries(birthdays).map((tableValue, tableKey) => 
-                    <Tab label={birthdayInfo[tableValue[0]].birthName} />
-                )}
-              </Tabs>
-            </AppBar>
-            <SwipeableViews
-              axis={'x'}
-              index={value}
-              onChangeIndex={handleChangeIndex}
-              >
-              {Object.entries(birthdays).map((tableValue, tableKey) => 
-                  <TabContainer>
-                    <Table key={tableKey} className={classes.table} aria-label="simple table">
-                      <TableHead>
-                        <TableRow>
-                          <TableCell>{birthdayInfo[tableValue[0]].header1}</TableCell>
-                          <TableCell align="right">{birthdayInfo[tableValue[0]].header2}</TableCell>
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {Object.entries(tableValue[1]).map((value, key) =>
-                            <TableRow key={key}>
-                              <TableCell align="left">{parseTimeSinceBirthday(value,tableValue)}</TableCell>
-                              <TableCell align="right">{parseBirthdayDate(value)}</TableCell>
-                            </TableRow>
-                        )}
-                      </TableBody>
-                  </Table>
-                </TabContainer>)}
-            </SwipeableViews>
-          </>}
-        </>
-    );
+  const parseTimeSinceBirthday = (value,tableValue) => {
+    return parseInt(value[0]).toLocaleString('en-US') + " " + birthdayInfo[tableValue[0]].symbol
   }
+
+  const parseBirthdayDate = (value) => {
+    var a = moment(value[1])
+    return a.format('Do MMM YYYY')
+  }
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  function handleChangeIndex(index) {
+    setValue(index);
+  }
+
+  return (
+    <>  
+      {birthdays != null && 
+      <Paper elevation={3}>
+          <AppBar className={classes.bar} position="static" color="default">
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              variant={null}>
+              {Object.entries(birthdays).map((tableValue, tableKey) => 
+                  <Tab key={tableKey} label={birthdayInfo[tableValue[0]].birthName} />
+              )}
+            </Tabs>
+          </AppBar>
+          <SwipeableViews
+            axis={'x'}
+            index={value}
+            onChangeIndex={handleChangeIndex}>
+            {Object.entries(birthdays).map((tableValue, tableKey) => 
+                <TabContainer key={tableKey}>
+                  <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>{birthdayInfo[tableValue[0]].header1}</TableCell>
+                        <TableCell align="right">{birthdayInfo[tableValue[0]].header2}</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {Object.entries(tableValue[1]).map((rowValue, rowKey) =>
+                          <TableRow key={rowKey}>
+                            <TableCell align="left">{parseTimeSinceBirthday(rowValue,tableValue)}</TableCell>
+                            <TableCell align="right">{parseBirthdayDate(rowValue)}</TableCell>
+                          </TableRow>
+                      )}
+                    </TableBody>
+                </Table>
+              </TabContainer>)}
+          </SwipeableViews>
+          </Paper>}
+    </>
+  );
+}
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import 'date-fns';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import DateFnsUtils from '@date-io/date-fns';
+import Paper from '@material-ui/core/Paper';
 import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 import BirthTable from '../components/BirthTable'
 import { postData } from '../util/http'
 
 const api = "https://c3patn17z1.execute-api.eu-west-1.amazonaws.com/dev";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
 export default function MaterialUIPickers() {
+  const classes = useStyles();
   const [selectedDate, setSelectedDate] = React.useState(new Date('1991-01-11'));
   const [decimalBirthdays, setDecimalBirthdays] = React.useState(null);
 
@@ -29,20 +44,30 @@ export default function MaterialUIPickers() {
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <Grid container justify="space-around">
-        <KeyboardDatePicker
-          margin="normal"
-          id="date-picker-dialog"
-          label="Your birthday date"
-          format="dd/MM/yyyy"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'birthday date',
-          }}
-        />
-        {decimalBirthdays != null &&
-            <BirthTable birthdays={decimalBirthdays}/>
-        }
+        <Grid item xs={12}>
+          <Paper className={classes.control}>
+            <Grid container>
+              <Grid item xs={12} lg={4}>
+                <KeyboardDatePicker
+                  margin="normal"
+                  id="date-picker-dialog"
+                  label="Your birthday date"
+                  format="dd/MM/yyyy"
+                  value={selectedDate}
+                  onChange={handleDateChange}
+                  KeyboardButtonProps={{
+                    'aria-label': 'birthday date',
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} lg={8}>
+                {decimalBirthdays != null &&
+                  <BirthTable birthdays={decimalBirthdays}/>
+                }
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
       </Grid>
     </MuiPickersUtilsProvider>
   );
